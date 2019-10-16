2b7095a8de5e95aad82d6e22e9a13253bb0057db
Dan Boehmke <Daniel@webappclouds.com>
2019-10-16 11:41:33 -0400

    Fixes issues

--- app/src/main/java/com/webappclouds/abcbuilders/Navigation2.java
+++ app/src/main/java/com/webappclouds/abcbuilders/Navigation2.java
@@ -1,27 +1,22 @@
 package com.webappclouds.abcbuilders;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import android.view.Menu;
 import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import com.google.android.material.navigation.NavigationView;
-
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.Menu;
-
-public class Navigation extends AppCompatActivity {
+public class Navigation2 extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
 
